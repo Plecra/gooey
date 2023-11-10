1038be5033a6a04ef726bbfbb767297d2fc244a9
kl-botsu <botsu@khonsulabs.com>
2023-11-10 20:23:35 +0000

    Deploying to gh-pages from @ khonsulabs/gooey@e683b7d31f0fa62601998d8209837afb285040e3 🚀

--- main/gooey/widgets/sidebar-items.js
+++ main/gooey/widgets/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["button","scroll","stack"],"struct":["Align","Canvas","Expand","Input","Label","Resize","Space","Style","TileMap"]};
+window.SIDEBAR_ITEMS = {"mod":["button","label","scroll","stack"],"struct":["Align","Canvas","Expand","Input","Resize","Space","Style","TileMap"]};
