c8cca796a670c447ba75c8acb101bcd8605a9d3b
kl-botsu <botsu@khonsulabs.com>
2023-11-21 03:51:57 +0000

    Deploying to gh-pages from @ khonsulabs/gooey@2201f2c83b87efd7bf2eab76d65d9118b0ab7d3d 🚀

--- main/gooey/widgets/slider/sidebar-items.js
+++ main/gooey/widgets/slider/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["InactiveTrackColor","KnobColor","KnobSize","MinimumSliderSize","Slider","TrackColor","TrackSize"],"trait":["Slidable"]};
+window.SIDEBAR_ITEMS = {"struct":["InactiveTrackColor","KnobColor","KnobSize","MinimumSliderSize","Slider","TrackColor","TrackSize"],"trait":["Slidable","SliderValue"]};
