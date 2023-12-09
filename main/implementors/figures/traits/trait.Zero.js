e672f82c1d87605f2a0a3074722838d0ef6ee465
kl-botsu <botsu@khonsulabs.com>
2023-12-09 21:21:44 +0000

    Deploying to gh-pages from @ khonsulabs/gooey@35576f92143958346c51a8530a77d7fd6eb50f2c 🚀

--- main/implementors/figures/traits/trait.Zero.js
+++ main/implementors/figures/traits/trait.Zero.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"gooey":[["impl Zero for <a class=\"enum\" href=\"gooey/styles/enum.Dimension.html\" title=\"enum gooey::styles::Dimension\">Dimension</a>"]]
+"gooey":[["impl Zero for <a class=\"enum\" href=\"gooey/styles/enum.Dimension.html\" title=\"enum gooey::styles::Dimension\">Dimension</a>"],["impl Zero for <a class=\"struct\" href=\"gooey/animation/struct.ZeroToOne.html\" title=\"struct gooey::animation::ZeroToOne\">ZeroToOne</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
