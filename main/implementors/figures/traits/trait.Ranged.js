490e2d1004672748eba6415cf06c92532a91afa1
kl-botsu <botsu@khonsulabs.com>
2023-12-15 04:03:06 +0000

    Deploying to gh-pages from @ khonsulabs/gooey@d7d06e41faccd8bb422ffd67208a82d51933dc26 🚀

--- main/implementors/figures/traits/trait.Ranged.js
+++ main/implementors/figures/traits/trait.Ranged.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"gooey":[["impl Ranged for <a class=\"struct\" href=\"gooey/animation/struct.ZeroToOne.html\" title=\"struct gooey::animation::ZeroToOne\">ZeroToOne</a>"],["impl Ranged for <a class=\"enum\" href=\"gooey/window/enum.ThemeMode.html\" title=\"enum gooey::window::ThemeMode\">ThemeMode</a>"]]
+"gooey":[["impl Ranged for <a class=\"enum\" href=\"gooey/window/enum.ThemeMode.html\" title=\"enum gooey::window::ThemeMode\">ThemeMode</a>"],["impl Ranged for <a class=\"struct\" href=\"gooey/animation/struct.ZeroToOne.html\" title=\"struct gooey::animation::ZeroToOne\">ZeroToOne</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
